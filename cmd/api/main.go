a2615cfd841d73d9236b65c9fe72cefb23e5786e
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2024-03-07 22:26:02 -0800

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,28 +5,19 @@ import (
     "net/http"
     "os"
     "strconv"
-
-    "github.com/redis/go-redis/v9"
-    "github.com/go-chi/chi/v5"
-    "github.com/dash-xd/logma/internal/app"
+    "github.com/dash-xd/logma2/internal/app"
 )
 
-func main() {
-    fmt.Printf("Starting server...\n")
-    redisClient := redis.NewClient(&redis.Options{
-        Addr:     os.Getenv("REDIS_URI"),
-        Password: os.Getenv("REDISCLI_AUTH"),
-        DB:       0,
-    })
-    defer redisClient.Close()
-    entityRegistrar := entity.NewRedisEntityRegistrar(redisClient)
-    router := app.NewRouter(redisClient, entityRegistrar)
-    port := getPortFromArgs()
-    addr := fmt.Sprintf(":%d", port)
-    fmt.Printf("Go Server is listening on http://localhost%s\n", addr)
-    if err := http.ListenAndServe(addr, router); err != nil {
-        panic(err)
-    }
+func main(){
+   fmt.Printf("starting server ... ")
+   router := app.NewRouter()
+   port := getPortFromArgs()
+   addr := fmt.Sprintf(":%d", port)
+   fmt.Printf("Go Server is listening on http://localhost%s\n", addr)
+   err := http.ListenAndServe(addr, router)
+   if err != nil {
+     panic(err)
+   }
 }
 
 func getPortFromArgs() int {
